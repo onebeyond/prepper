214b831f249fc56b1095b5117f5114527b7da146
Stephen Cresswell <stephen.cresswell@guidesmiths.com>
2016-05-08 23:08:31 +0100

    OS decorator

---
+++ tests/handlers/System.tests.js
@@ -0,0 +1,37 @@
+var assert = require('chai').assert
+var lib = require('../..')
+var semver = require('semver')
+var Logger = lib.Logger
+var Repo = lib.handlers.Repo
+var System = lib.handlers.System
+
+describe('System Decorator', function() {
+
+    var repo = new Repo()
+    var system = new System()
+    var logger = new Logger()
+
+    beforeEach(function() {
+        logger.on('message', system.handle)
+        system.on('message', repo.handle)
+    })
+
+    afterEach(function() {
+        repo.clear()
+        system.removeAllListeners()
+        logger.removeAllListeners()
+    })
+
+    it('should decorate events with details from the os', function() {
+        logger.debug('meh')
+
+        var event = repo.first()
+        assert.ok(event.os.hostname)
+        assert.ok(event.os.type)
+        assert.ok(event.os.platform)
+        assert.ok(event.os.release)
+        assert.ok(event.os.mem.total)
+        assert.ok(event.os.mem.free)
+        assert.ok(event.os.loadavg)
+    })
+})
