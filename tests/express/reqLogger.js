b73133ead4148099648e58bb0132f200beaa2fba
Stephen Cresswell <stephen.cresswell@guidesmiths.com>
2016-05-10 00:18:47 +0100

    Tidying request logger

--- tests/express/reqLogger.js
+++ tests/express/reqLogger.js
@@ -13,9 +13,10 @@ module.exports = function(req, res) {
     sequence.on('message', req.app.locals.logger.log)
 
     onHeaders(res, function() {
-        if (res.statusCode < 400) logger.info(req.url, { response: { statusCode: res.statusCode } })
-        else if (res.statusCode < 500) logger.warn(req.url, { response: { statusCode: res.statusCode } })
-        else logger.error(req.url, { response: { statusCode: res.statusCode } })
+        var response = { response: { statusCode: res.statusCode } }
+        if (res.statusCode < 400) logger.info(req.url, response)
+        else if (res.statusCode < 500) logger.warn(req.url, response)
+        else logger.error(req.url, response)
     })
 
     res.locals.logger = logger
